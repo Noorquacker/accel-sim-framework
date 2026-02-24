160a8f0ad56dcfe5e4ed782e2b5b042a505a666e
Noorquacker <noorquacker@nqind.com>
2026-02-24 09:27:39 -0600

    add homework assignment's apps and jobs

---
+++ run_sms.sh
@@ -0,0 +1,30 @@
+# not gonna do loops
+# 60 SM
+
+./util/job_launching/run_simulations.py -B ece511-rodinia-3.1 -C ECE511_TITANV-SASS-SM_60 -T ./hw_run/rodinia-3.1/11.0/ -N ece511-rodinia-sm60
+./util/job_launching/run_simulations.py -B ece511-parboil -C ECE511_TITANV-SASS-SM_60 -T ./hw_run/parboil/11.0/ -N ece511-parboil-sm60
+./util/job_launching/run_simulations.py -B ece511-polybench -C ECE511_TITANV-SASS-SM_60 -T ./hw_run/polybench/11.0/ -N ece511-polybench-sm60
+
+# 100 SM
+
+./util/job_launching/run_simulations.py -B ece511-rodinia-3.1 -C ECE511_TITANV-SASS-SM_100 -T ./hw_run/rodinia-3.1/11.0/ -N ece511-rodinia-sm100
+./util/job_launching/run_simulations.py -B ece511-parboil -C ECE511_TITANV-SASS-SM_100 -T ./hw_run/parboil/11.0/ -N ece511-parboil-sm100
+./util/job_launching/run_simulations.py -B ece511-polybench -C ECE511_TITANV-SASS-SM_100 -T ./hw_run/polybench/11.0/ -N ece511-polybench-sm100
+
+# 120 SM
+
+./util/job_launching/run_simulations.py -B ece511-rodinia-3.1 -C ECE511_TITANV-SASS-SM_120 -T ./hw_run/rodinia-3.1/11.0/ -N ece511-rodinia-sm120
+./util/job_launching/run_simulations.py -B ece511-parboil -C ECE511_TITANV-SASS-SM_120 -T ./hw_run/parboil/11.0/ -N ece511-parboil-sm120
+./util/job_launching/run_simulations.py -B ece511-polybench -C ECE511_TITANV-SASS-SM_120 -T ./hw_run/polybench/11.0/ -N ece511-polybench-sm120
+
+# 160 SM
+
+./util/job_launching/run_simulations.py -B ece511-rodinia-3.1 -C ECE511_TITANV-SASS-SM_160 -T ./hw_run/rodinia-3.1/11.0/ -N ece511-rodinia-sm160
+./util/job_launching/run_simulations.py -B ece511-parboil -C ECE511_TITANV-SASS-SM_160 -T ./hw_run/parboil/11.0/ -N ece511-parboil-sm160
+./util/job_launching/run_simulations.py -B ece511-polybench -C ECE511_TITANV-SASS-SM_160 -T ./hw_run/polybench/11.0/ -N ece511-polybench-sm160
+
+# 200 SM
+
+./util/job_launching/run_simulations.py -B ece511-rodinia-3.1 -C ECE511_TITANV-SASS-SM_200 -T ./hw_run/rodinia-3.1/11.0/ -N ece511-rodinia-sm200
+./util/job_launching/run_simulations.py -B ece511-parboil -C ECE511_TITANV-SASS-SM_200 -T ./hw_run/parboil/11.0/ -N ece511-parboil-sm200
+./util/job_launching/run_simulations.py -B ece511-polybench -C ECE511_TITANV-SASS-SM_200 -T ./hw_run/polybench/11.0/ -N ece511-polybench-sm200
